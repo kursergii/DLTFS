7eb90e154e638f2fef8f12b9c0ffc2fb086ffa34
Sergii Kurlov <ksslog@mm.st>
2026-03-13 12:29:04 +0100

    Add note about main.moc inclusion for Qt's meta-object system

--- main.cpp
+++ main.cpp
@@ -14,3 +14,6 @@ int main(int argc, char *argv[])
     w.show();
     return a.exec();
 }
+
+// Note: The "main.moc" include is necessary for Qt's meta-object system to work properly
+// when using signals and slots in the same file. It should be included at the end of the source file.
